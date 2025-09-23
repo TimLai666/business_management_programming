290ff179cb0085b9537e3fa663586bbc086ee4bc
TimLai666 <tim930102@icloud.com>
2025-09-23 15:11:26 +0800

    新增作業五的科系查詢功能

--- ex3_U11151013.py
+++ ex3_U11151013.py
@@ -24,13 +24,26 @@
 # eng_word: str = input("請輸入一個英文單字？")
 # print(translate_dict.get(eng_word, "找不到這個生字！"))
 
-# U11151013 賴廷榛 ex3 作業四 2025/09/23
-menu: dict[str, float] = {}
-print("請輸入3種餐點的價目表來建立菜單")
+# # U11151013 賴廷榛 ex3 作業四 2025/09/23
+# menu: dict[str, float] = {}
+# print("請輸入3種餐點的價目表來建立菜單")
+# for i in range(3):
+#     food_name: str = input(f"第{i + 1}種餐點？")
+#     food_price: float = float(input(f"第{i + 1}種餐點的價格？"))
+#     menu[food_name] = food_price
+# print()
+# food_to_search: str = input("請輸入想查詢的餐點名稱？")
+# print(menu.get(food_to_search, "價目表中沒有這個餐點！"))
+
+# U11151013 賴廷榛 ex3 作業五 2025/09/23
+dept_dict: dict[str, str] = {}
+print("請輸入請輸入3個科系的科系代碼")
 for i in range(3):
-    food_name: str = input(f"第{i + 1}種餐點？")
-    food_price: float = float(input(f"第{i + 1}種餐點的價格？"))
-    menu[food_name] = food_price
+    dept_name: str = input(f"第{i + 1}個科系？")
+    dept_code: str = input(f"第{i + 1}個科系的代碼？")
+    dept_dict[dept_code] = dept_name
 print()
-food_to_search: str = input("請輸入想查詢的餐點名稱？")
-print(menu.get(food_to_search, "價目表中沒有這個餐點！"))
+student_id: str = input("請輸入想查詢的學號？")
+student_dept_code: str = student_id[4:6]
+student_dept: str | None = dept_dict.get(student_dept_code, None)
+print(f"此學號的科系為{student_dept}" if student_dept else "名單中沒有這個學號的科系代碼！")
