232f763b85464a5b50238ac5924f23e42b9b8d4a
TimLai666 <tim930102@icloud.com>
2025-09-23 14:36:32 +0800

    移除數字輸入與排序功能的註解，新增文字輸入功能並移除特定字元

--- ex3_U11151013.py
+++ ex3_U11151013.py
@@ -1,7 +1,14 @@
 # U11151013 賴廷榛 ex3 作業一 2025/09/23
-nums: list[float] = []
-for i in range(3):
-    num: float = float(input(f"數字{i + 1}？"))
-    nums.append(num)
-nums.sort()
-print(nums)
+# nums: list[float] = []
+# for i in range(3):
+#     num: float = float(input(f"數字{i + 1}？"))
+#     nums.append(num)
+# nums.sort()
+# print(nums)
+
+# U11151013 賴廷榛 ex3 作業二 2025/09/23
+text: str = input("輸入一段文字？")
+chars: set[str] = set(text)
+for ch_to_remove in ["，", "。"]:
+    chars.remove(ch_to_remove)
+print(chars)
