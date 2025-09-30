48d0c59731513cb41a9e3aa1dd55797bbacfa1a9
TimLai666 <tim930102@icloud.com>
2025-09-30 16:06:53 +0800

    重構作業四的郵資計算邏輯，移除不必要的程式碼並新增找零計算功能

--- ex4_U11151013.py
+++ ex4_U11151013.py
@@ -32,31 +32,60 @@
 #     cost = 110
 # print(f"重量{weight}公斤，"+(f"所需郵資為{cost}元" if cost != -1 else "超過20公斤無法寄送"))
 
-# U11151013 賴廷榛 ex4 作業四 2025/09/30
-delivery_method: str = input("請問包裹配送方式(1.宅配 2.i郵箱取貨)？")
-if not delivery_method in {"1", "2"}:
-    print("包裹配送方式必須輸入1或2！")
-    exit(1)
+# # U11151013 賴廷榛 ex4 作業四 2025/09/30
+# delivery_method: str = input("請問包裹配送方式(1.宅配 2.i郵箱取貨)？")
+# if not delivery_method in {"1", "2"}:
+#     print("包裹配送方式必須輸入1或2！")
+#     exit(1)
 
-cost: int = -1
-package_weight: float = float(input("請問包裹重量為多少公斤？"))
-if delivery_method == "1":
-    if package_weight <= 5:
-        cost = 70
-    elif package_weight <= 10:
-        cost = 90
-    elif package_weight <= 15:
-        cost = 110
-    elif package_weight <= 20:
-        cost = 130
+# cost: int = -1
+# package_weight: float = float(input("請問包裹重量為多少公斤？"))
+# if delivery_method == "1":
+#     if package_weight <= 5:
+#         cost = 70
+#     elif package_weight <= 10:
+#         cost = 90
+#     elif package_weight <= 15:
+#         cost = 110
+#     elif package_weight <= 20:
+#         cost = 130
+# else:
+#     if package_weight <= 5:
+#         cost = 48
+#     elif package_weight <= 10:
+#         cost = 68
+#     elif package_weight <= 15:
+#         cost = 88
+#     elif package_weight <= 20:
+#         cost = 113
+# print(f"{"宅配" if delivery_method == "1" else "i郵箱取貨"}重量{package_weight}公斤，" +
+#       (f"所需郵資為{cost}元" if cost != -1 else "超過20公斤無法寄送"))
+
+# U11151013 賴廷榛 ex4 作業五 2025/09/30
+denominations: list[int] = sorted([100, 50, 10, 5, 1], reverse=True)
+checkout_amount: int = int(input("請輸入結帳金額？"))
+pay_amount: int = int(input("請輸入顧客支付金額？"))
+if pay_amount == checkout_amount:
+    print("不需要找零")
+elif pay_amount < checkout_amount:
+    print(f"所支付的金額不夠{checkout_amount - pay_amount}元")
 else:
-    if package_weight <= 5:
-        cost = 48
-    elif package_weight <= 10:
-        cost = 68
-    elif package_weight <= 15:
-        cost = 88
-    elif package_weight <= 20:
-        cost = 113
-print(f"{"宅配" if delivery_method == "1" else "i郵箱取貨"}重量{package_weight}公斤，" +
-      (f"所需郵資為{cost}元" if cost != -1 else "超過20公斤無法寄送"))
+    total_change: int = pay_amount - checkout_amount
+    change_str: str = f"找零{total_change}元："
+    change_per_denomination: dict[int, int] = {}
+    for denomination in denominations:
+        if total_change >= denomination:
+            num_denomination: int = total_change // denomination
+            change_per_denomination[denomination] = num_denomination
+            total_change -= denomination * num_denomination
+
+    remaining_iter = len(change_per_denomination)
+    for denomination, count in change_per_denomination.items():
+        if denomination >= 100:
+            change_str += f"{count}張{denomination}元紙鈔"
+        else:
+            change_str += f"{count}個{denomination}元硬幣"
+        remaining_iter -= 1
+        if remaining_iter > 0:
+            change_str += " "
+    print(change_str)
