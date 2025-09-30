5642bfcfef0962daec433232be4fcd2fa308b601
TimLai666 <tim930102@icloud.com>
2025-09-30 15:08:54 +0800

    更新作業一、二、三的作者編號

--- ex4_U11151013.py
+++ ex4_U11151013.py
@@ -1,4 +1,4 @@
-# # U11051001 賴廷榛 ex4 作業一 2025/09/30
+# # U11151013 賴廷榛 ex4 作業一 2025/09/30
 # side_lengths: list[float] = []
 # for i in range(3):
 #     length: float = float(input(f"請輸入三角形邊長{i + 1}的長度？"))
@@ -12,11 +12,11 @@
 #     # 如果for迴圈沒有被break，則執行else
 #     print("可構成三角形")
 
-# U11051001 賴廷榛 ex4 作業二 2025/09/30
+# U11151013 賴廷榛 ex4 作業二 2025/09/30
 year: int = int(input("請輸入一個年度："))
 is_leap: bool = False
 if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
     is_leap = True
 print(f"{year}是閏年" if is_leap else f"{year}是平年")
 
-# U11051001 賴廷榛 ex4 作業三 2025/09/30
+# U11151013 賴廷榛 ex4 作業三 2025/09/30
