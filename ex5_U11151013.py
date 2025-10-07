27417a0bb1d5c3c4d18d08c82307503d9e938a84
TimLai666 <tim930102@icloud.com>
2025-10-07 15:37:06 +0800

    新增作業二，實作計算1到1000之間是指定整數M或N的倍數的數字個數

--- ex5_U11151013.py
+++ ex5_U11151013.py
@@ -15,3 +15,12 @@ factorial: int = 1
 for i in range(1, input_int + 1):
     factorial *= i
 print(f"{input_int}! = {factorial}")
+
+# U11151013 賴廷榛 ex5 作業二 2025/10/07
+input_m: int = int(input("請輸入第一個整數M："))
+input_n: int = int(input("請輸入第二個整數N："))
+num_multiples_of_m_or_n: int = 0
+for i in range(1, 1001):
+    if i % input_m == 0 or i % input_n == 0:
+        num_multiples_of_m_or_n += 1
+print(f"1到1000之間是{input_m}或{input_n}的倍數的數字共有{num_multiples_of_m_or_n}個")
