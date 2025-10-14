e4f465b2ba19f3bb70679bcc0d2c2a4a1a2a45a6
TimLai666 <tim930102@icloud.com>
2025-10-14 14:06:48 +0800

    新增作業三，實作整數階乘計算功能

--- ex6_U11151013.py
+++ ex6_U11151013.py
@@ -22,3 +22,19 @@ if has_digit and has_alpha:
     print(f"此字串{input_str}符合規定")
 else:
     print("輸入的字串沒有同時包含數字與英文字")
+
+# U11151013 賴廷榛 ex6 作業三 2025/10/14
+input_int_m: int = int(input("請輸入一個整數:"))
+max_n: int = 0
+min_n: int = 0
+for i in range(1, input_int_m + 1):
+    factorial: int = 1
+    for j in range(1, i + 1):
+        factorial *= j
+    if factorial < input_int_m:
+        max_n = i
+    elif min_n == 0:
+        min_n = i
+        break
+print(f"{max_n}!<{input_int_m}")
+print(f"{min_n}!>={input_int_m}")
