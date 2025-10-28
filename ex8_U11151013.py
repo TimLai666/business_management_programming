bf6e53e37f232422b766e8630bce81b6668a3f07
TimLai666 <tim930102@icloud.com>
2025-10-28 14:36:52 +0800

    新增計算公式的實作，包含遞迴函數及用戶輸入邏輯

---
+++ ex8_U11151013.py
@@ -0,0 +1,9 @@
+# U11151013 賴廷榛 ex8 作業一 2025/10/28
+def f(n: int) -> int:
+    if n == 1:
+        return 0
+    return n*(n-1)+f(n-1)
+
+
+n: int = int(input("請輸入一個整數？"))
+print(f"1*2+2*3+...+{(n-1)}*{n}={f(n)}")
