7c70f4228f3eafa40c966711228e454e1971f832
TimLai666 <tim930102@icloud.com>
2025-11-25 16:07:37 +0800

    Refactor code structure for improved readability and maintainability

--- ex9_U11151013.py
+++ ex9_U11151013.py
@@ -7,4 +7,25 @@ with open("report.txt", "w", encoding="utf-8") as file:
     file.write(f"居住縣市：{city}\n")
     file.write(f"電話：{phone}\n")
 
+# %% U11151013 賴廷榛 ex9 作業二 2025/11/4
+import csv
+fields: list = []
+rows: list = []
+with open("105_traffic.csv", "r", encoding="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile)
+    fields = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+print(f"共有{len(rows)}筆資料")
+print(fields)
+print("前五筆資料如下：")
+for row in rows[:5]:
+    print(row)
+print("1~12月各月份發生車禍的次數如下：")
+month_count: list = []
+for month in range(1, 13):
+    count: int = sum(1 for row in rows if int(row[1]) == month)
+    month_count.append(count)
+
+print(month_count)
 # %%
