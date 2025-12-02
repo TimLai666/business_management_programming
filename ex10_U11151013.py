699f77b968f428a1c17754fbbe776dba332bcfde
TimLai666 <tim930102@icloud.com>
2025-12-02 15:42:22 +0800

    Add student data analysis script with BMI calculations and sorting features
    - Created a new Python script to manage and analyze student data.
    - Included student attributes: 姓名, 身高, 體重, and 戶籍.
    - Calculated BMI for each student and added it to the DataFrame.
    - Implemented functionality to display all student data, sort by weight, and calculate average height and weight.
    - Filtered and displayed students who are overweight (BMI >= 24.0) and underweight (BMI < 18.5).

---
+++ ex10_U11151013.py
@@ -0,0 +1,38 @@
+# %% U11151013 賴廷榛 ex10 作業一 2025/12/2
+import pandas as pd
+
+student: dict[str, list] = {
+    "姓名":["Tom", "Alice", "John","Cathy"],
+    "身高":[181, 165, 176, 154],
+    "體重":[101, 54, 72, 42]
+}
+data: pd.DataFrame = pd.DataFrame(student)
+
+data["戶籍"] = ["高雄", "台南", "台北", "台中"]
+data["BMI"] = data["體重"] / ((data["身高"] / 100) ** 2)
+
+# (1)所有學生的資料
+print("所有學生資料")
+print(data)
+print()
+
+# (2)所有學生資料依照體重由大到小排序
+print("所有學生資料依照體重由大到小排序")
+print(data.sort_values(by="體重", ascending=False))
+print()
+
+# (3)平均身高與平均體重
+print(f"平均身高：{data['身高'].mean():.2f} cm")
+print(f"平均體重：{data['體重'].mean():.2f} kg")
+print()
+
+# (4)列出過重者(BMI>=24.0)
+print("過重者(BMI>=24.0)")
+print(data[data["BMI"] >= 24.0])
+print()
+
+# (5)列出過輕者(BMI<18.5)
+print("過輕者(BMI<18.5):")
+print(data[data["BMI"] < 18.5])
+print()
+# %%
