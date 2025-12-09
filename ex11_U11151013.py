ef46098bf657ebe9f50d61eb91e2a5aaa3251b0c
TimLai666 <tim930102@icloud.com>
2025-12-09 16:12:00 +0800

    Refactor code structure for improved readability and maintainability

--- ex11_U11151013.py
+++ ex11_U11151013.py
@@ -16,4 +16,41 @@ plt.title("Sales Report")
 plt.legend()
 plt.grid(color="blue", linewidth=1, alpha=0.5)
 plt.show()
+
+# %% U11151013 賴廷榛 ex11 作業二 2025/12/9
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.font_manager as fm
+fm.fontManager.addfont('TaipeiSansTCBeta-Regular.ttf')
+matplotlib.rc('font', family='Taipei Sans TC Beta')
+
+# 讀入資料集檔案
+df = pd.read_csv('109年台北市出生人數.csv',encoding='big5')
+print(df)
+# 用matplotlib中的 bar()函數畫長條圖
+datax = list(df['行政區']) # datax = df['行政區'] 也可以
+datay = list(df['一月'])
+
+#用datax, datay化長條圖，標記為一月
+plt.bar(datax, datay, label='一月', color='blue')
+
+# 設定標頭
+plt.title('109年台北市各行政區一月出生人口數')
+
+# 設定 x 軸標頭
+plt.xlabel('行政區')
+
+# 設定 y 軸標頭
+plt.ylabel('出生人口數')
+
+# 設定右上角說明圖示字體
+plt.legend()
+
+#x軸標題轉45度
+plt.xticks(rotation=45) 
+# 顯示圖表
+plt.show()
+#旋轉X軸標籤方向
+
 # %%
